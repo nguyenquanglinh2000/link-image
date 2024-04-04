8980e3a281100eb891cd601fdf7c7fc60f8fdef8
Lihn <quanglinhk2000@gmail.com>
2024-04-04 23:30:27 +0700

    1

--- image-fb.php
+++ image-fb.php
@@ -1,24 +1,15 @@
-<html xmlns="http://www.w3.org/1999/xhtml" lang="vi" prefix="og:http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
-
+<html xmlns="https://www.w3.org/1999/xhtml" lang="vi" prefix="og:https://ogp.me/ns# fb: https://ogp.me/ns/fb#">
 <head>
-
-    <meta property="og:locale" content="vi_VN" />
-
-    <meta property="og:description" content="" />
-
-    <meta property="og:site_name" content="" />
-
-    <meta property="og:title" content=" " />
-
-    <meta property="og:url" content="https://scontent.fhan20-1.fna.fbcdn.net/v/t39.30808-6/328835367_714261113501351_1809819840977251579_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gayRZMSgoO0Ab5rI_BD&_nc_ht=scontent.fhan20-1.fna&oh=00_AfDITg4_5IsBbqTAugKWK7EFkJhtcY5Nzs-mMP9agj2Ymg&oe=6614AE17" />
-
-    <meta property="og:image" content="https://scontent.fhan20-1.fna.fbcdn.net/v/t39.30808-6/328835367_714261113501351_1809819840977251579_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gayRZMSgoO0Ab5rI_BD&_nc_ht=scontent.fhan20-1.fna&oh=00_AfDITg4_5IsBbqTAugKWK7EFkJhtcY5Nzs-mMP9agj2Ymg&oe=6614AE17" />
+<meta property="og:locale" content="vi_VN" />
+<meta property="og:description" content="" />
+<meta property="og:site_name" content="" />
+<meta property="og:title" content=" " />
+<meta property="og:url" content="https://scontent.fhan20-1.fna.fbcdn.net/v/t39.30808-6/326417676_854268225658936_4148520246382417720_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=3Wg-OUm-js0Ab6m0Pm4&_nc_ht=scontent.fhan20-1.fna&oh=00_AfCxLkYJv6k4wu9OhojqoLAypGBhTt4jNvILmuAz_rBr_w&oe=6614B4A9" />
+<meta property="og:image" content="https://scontent.fhan20-1.fna.fbcdn.net/v/t39.30808-6/326417676_854268225658936_4148520246382417720_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=3Wg-OUm-js0Ab6m0Pm4&_nc_ht=scontent.fhan20-1.fna&oh=00_AfCxLkYJv6k4wu9OhojqoLAypGBhTt4jNvILmuAz_rBr_w&oe=6614B4A9" />
 </head>
-
 <body>
-    <script>
-        window.location.replace("https://www.facebook.com/profile.php?id=100026234350121")
-    </script>
+<script>
+window.location.replace("https://www.facebook.com/nguyenquanglinh.00/");
+</script>
 </body>
-
 </html>
