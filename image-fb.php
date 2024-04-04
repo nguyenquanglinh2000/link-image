c503e2ee17b6545e19450351710b69da65b7d310
Lihn <quanglinhk2000@gmail.com>
2024-04-04 22:10:21 +0700

    first commit

---
+++ image-fb.php
@@ -0,0 +1,24 @@
+<html xmlns="http: //www.w3.org/1999/xntml" lang="vi" prefix="og:http://ogp.me/ns# fb: http: //ogp.me/ns/fb#">
+
+<head>
+
+    <meta property="og:locale" content="vi_VN" />
+
+    <meta property="og:description" content="" />
+
+    <meta property="og:site_name" content="" />
+
+    <meta property="og:title" content=" " />
+
+    <meta property="og:url" content="https://scontent.fhan20-1.fna.fbcdn.net/v/t39.30808-6/328835367_714261113501351_1809819840977251579_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gayRZMSgoO0Ab5rI_BD&_nc_ht=scontent.fhan20-1.fna&oh=00_AfDITg4_5IsBbqTAugKWK7EFkJhtcY5Nzs-mMP9agj2Ymg&oe=6614AE17" />
+
+    <meta property="og:image" content="https://scontent.fhan20-1.fna.fbcdn.net/v/t39.30808-6/328835367_714261113501351_1809819840977251579_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gayRZMSgoO0Ab5rI_BD&_nc_ht=scontent.fhan20-1.fna&oh=00_AfDITg4_5IsBbqTAugKWK7EFkJhtcY5Nzs-mMP9agj2Ymg&oe=6614AE17" />
+</head>
+
+<body>
+    <script>
+        window.location.replace("https://www.facebook.com/profile.php?id=100026234350121")
+    </script>
+</body>
+
+</html>
